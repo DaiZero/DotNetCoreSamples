0dedb0464aa43c3092d145e4b8a11e66da36a685
独孤一剑 <dzd624175217@gmail.com>
2018-11-07 15:32:14 +0800

    添加rabbitmq使用例子

---
+++ CoreSample.RabbitMQ.Customer/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace CoreSample.RabbitMQ.Customer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("输入队列名称：");
+            var queuename = Console.ReadLine().Trim();
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";//RabbitMQ服务在本地运行
+            factory.UserName = "test";//用户名
+            factory.Password = "test";//密码
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        //var queuename = "test003";//消息队列的名称
+                        bool durable = true;//队列是否持久化
+                        channel.QueueDeclare(queuename, durable, false, false, null);//创建一个消息队列,如果有则不创建
+                        channel.BasicQos(0, 1, false);//公平分发配置,使得每个Consumer在同一个时间点最多处理一个Message【在创建消费者之前设置，才会有效】
+
+                        var consumer = new EventingBasicConsumer(channel);//创建消费者
+                        channel.BasicConsume(queuename, false, consumer);
+                        consumer.Received += (model, ea) =>
+                        {
+                            var body2 = ea.Body;
+                            var message2 = Encoding.UTF8.GetString(body2);
+                            int dots = message2.Split('.').Length - 1;
+                            Thread.Sleep(dots * 1000);
+                            Console.WriteLine("已接收： {0}", message2);
+                            channel.BasicAck(ea.DeliveryTag, false);
+                        };
+                        Console.ReadLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CoreSample.RabbitMQ.Producer/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace CoreSample.RabbitMQ.Producer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("输入队列名称：");
+            var queuename = Console.ReadLine().Trim();
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";//RabbitMQ服务在本地运行
+            factory.UserName = "test";//用户名
+            factory.Password = "test";//密码
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        bool durable = true;//队列是否持久化
+                        channel.QueueDeclare(queuename, durable, false, false, null);//创建一个消息队列,如果有则不创建
+                        while(true)
+                        {
+                            Console.Write("输入信息：");
+                            var message = Console.ReadLine().Trim();
+                            var body = Encoding.UTF8.GetBytes(message);
+                            var properties = channel.CreateBasicProperties();
+                            properties.Persistent = true;//消息持久化
+                            channel.BasicPublish("", queuename, properties, body); //开始传递
+                        }
+                        //Console.WriteLine("已发送： {0}", message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
